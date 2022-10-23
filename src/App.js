9f73d17f81979f29959d10e70f74f75fd0329cc0
Emil Velasquez <evelas10@utexas.edu>
2022-10-23 17:00:35 -0500

    imported three0

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
+import { isLoggedIn } from "@three0dev/js-sdk/auth";
 import { ToastProvider } from "react-toast-notifications";
 import SearchProducts from "./SearchProducts";
 import Payment from "./Payment";
@@ -16,82 +17,82 @@ import { Elements } from "@stripe/react-stripe-js";
 import PaymentNew from "./PaymentNew";
 
 const promise = loadStripe(
-  "pk_test_51HPuCnEo7NoTqqusCx3eB7c7qTsapXKw15MshI4XePeBnqOHInBIKiHUtYTVpnwQq53x1jtdUpxDlhGmo7HGVAFl00nRV2jT0Q"
+    "pk_test_51HPuCnEo7NoTqqusCx3eB7c7qTsapXKw15MshI4XePeBnqOHInBIKiHUtYTVpnwQq53x1jtdUpxDlhGmo7HGVAFl00nRV2jT0Q"
 );
 
 function App() {
-  const [{ user }, dispatch] = useStateValue();
+    const [{ user }, dispatch] = useStateValue();
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        //logged in
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                //logged in
+                dispatch({
+                    type: "SET_USER",
+                    user: authUser,
+                });
+            } else {
+                //logged out
+                dispatch({
+                    type: "SET_USER",
+                    user: null,
+                });
+            }
         });
-      } else {
-        //logged out
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
-      }
-    });
 
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
-  useEffect(() => {
-    if (localStorage.getItem("basket")) {
-      var cart = JSON.parse(localStorage.getItem("basket"));
-      dispatch({
-        type: "SET_BASKET",
-        basket: cart.basket,
-      });
-    }
-  }, []);
+    useEffect(() => {
+        if (localStorage.getItem("basket")) {
+            var cart = JSON.parse(localStorage.getItem("basket"));
+            dispatch({
+                type: "SET_BASKET",
+                basket: cart.basket,
+            });
+        }
+    }, []);
 
-  return (
-    <ToastProvider>
-      <Router>
-        <div className="app">
-          <Switch>
-            <Route path="/orders">
-              <Header />
-              <Orders />
-            </Route>
-            <Route path="/checkout">
-              <Header />
-              <Checkout />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/search">
-              <Header />
-              <SearchProducts />
-            </Route>
-            <Route path="/payment">
-              <Header />
-              <Elements stripe={promise}>
-                <PaymentNew />
-              </Elements>
-              {/* <Elements stripe={promise}>
+    return (
+        <ToastProvider>
+            <Router>
+                <div className="app">
+                    <Switch>
+                        <Route path="/orders">
+                            <Header />
+                            <Orders />
+                        </Route>
+                        <Route path="/checkout">
+                            <Header />
+                            <Checkout />
+                        </Route>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path="/search">
+                            <Header />
+                            <SearchProducts />
+                        </Route>
+                        <Route path="/payment">
+                            <Header />
+                            <Elements stripe={promise}>
+                                <PaymentNew />
+                            </Elements>
+                            {/* <Elements stripe={promise}>
                 <Payment />
               </Elements> */}
-            </Route>
-            <Route path="/">
-              <Header />
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </ToastProvider>
-  );
+                        </Route>
+                        <Route path="/">
+                            <Header />
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </ToastProvider>
+    );
 }
 
 export default App;
---
+++ src/env.js
@@ -0,0 +1,9 @@
+export const env = {
+    config: {
+        "contractName": "amazon-bczdbmbgtr4qvfxeooohgs.evelas.testnet",
+        "projectId": "amazon-bczdbmbgtr4qvfxeooohgs",
+        "chainType": "NEAR_TESTNET"
+    },
+    usersDB: "/orbitdb/zdpuAm2QNGcdGWKBk258SbCsZ2y8VtN29cDhJTAC4hgxD7C8S/users",
+    productsDB: "/orbitdb/zdpuAmbMpcNHfjNSqzDD2JYSBMA4UQHLWszRrcbP24mH1ZsHR/products"
+}
