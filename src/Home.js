83aabfe92aaaf6a15375d802c1b269b6c8921241
Emil Velasquez <evelas10@utexas.edu>
2022-10-28 23:53:12 -0500

    Updated home page to use three0

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Product from "./Product";
-import data from "./products.json";
 import FlipMove from "react-flip-move";
-import { db } from "./firebase";
+import { Database } from "@three0dev/js-sdk";
 import { useStateValue } from "./StateProvider";
 import { CircularProgress } from "@material-ui/core";
+
+import { env } from "./env"
 
 // https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_2x._CB432458382_.jpg
 // https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg
@@ -15,25 +16,32 @@ function Home() {
   const [loading, setLoading] = useState(null);
 
   useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  const fetchProducts = async () => {
     setLoading(true);
-    db.collection("products").onSnapshot((snapshot) => {
-      var items = snapshot.docs.map((doc) => {
-        var { title, image, price, rating } = doc.data();
-        return {
-          title,
-          image,
-          price,
-          rating,
-          id: doc.id,
-        };
-      });
-      dispatch({
-        type: "SET_PRODUCTS",
-        products: items,
-      });
-      setLoading(false);
+
+    const docstore = await Database.DocStore(env.productsDB)
+
+    let productsSnapshot = docstore.get();
+
+    let items = productsSnapshot.map((product) => {
+      let { title, price, rating, image } = product;
+      return {
+        id: product._id,
+        title,
+        image,
+        price,
+        rating
+      }
     });
-  }, []);
+    dispatch({
+      type: "SET_PRODUCTS",
+      products: items
+    });
+    setLoading(false);
+  }
 
   return (
     <div className="home">
@@ -65,7 +73,7 @@ function Home() {
           >
             <div className="home__row">
               {products.map(({ id, title, price, rating, image }) => {
-                if (id !== "2sUCuKMDsy4WhkYc0uSD") {
+                if (id !== "8e45b8a7-3044-4731-9a67-b0a2b14a6841") {
                   return (
                     <Product
                       key={id}
@@ -83,7 +91,7 @@ function Home() {
 
           <div className="home__rowlarge">
             <Product
-              id="2sUCuKMDsy4WhkYc0uSD"
+              id="8e45b8a7-3044-4731-9a67-b0a2b14a6841"
               title="Samsung LC49HG90DMUXEN 48.9-inch Ultra Wide Curved Monitor (Black)"
               price={100000}
               rating={5}
