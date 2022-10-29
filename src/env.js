2b25266890806f8047a0974b6ed067691fe775aa
Emil Velasquez <evelas10@utexas.edu>
2022-10-28 23:23:26 -0500

    new productsDB address

--- src/env.js
+++ src/env.js
@@ -1,9 +1,9 @@
 export const env = {
-    config: {
-        "contractName": "amazon-bczdbmbgtr4qvfxeooohgs.evelas.testnet",
-        "projectId": "amazon-bczdbmbgtr4qvfxeooohgs",
-        "chainType": "NEAR_TESTNET"
-    },
-    usersDB: "/orbitdb/zdpuAm2QNGcdGWKBk258SbCsZ2y8VtN29cDhJTAC4hgxD7C8S/users",
-    productsDB: "/orbitdb/zdpuAmbMpcNHfjNSqzDD2JYSBMA4UQHLWszRrcbP24mH1ZsHR/products"
+  config: {
+    "contractName": "amazon-bczdbmbgtr4qvfxeooohgs.evelas.testnet",
+    "projectId": "amazon-bczdbmbgtr4qvfxeooohgs",
+    "chainType": "NEAR_TESTNET"
+  },
+  usersDB: "/orbitdb/zdpuAm2QNGcdGWKBk258SbCsZ2y8VtN29cDhJTAC4hgxD7C8S/users",
+  productsDB: "/orbitdb/zdpuAoTaE3vDKm7Aw2XmGNtndvLJ8aqp9m8gbzXctzW29XZ23/product"
 }
